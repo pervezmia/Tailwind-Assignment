11b734615cc6671a83496423cb0746677c956cc1
pervezmia <ahmedpervezkabir@gmail.com>
2025-11-03 22:21:16 +0600

    js file

--- index.js
+++
@@ -1,6 +0,0 @@
-const htmltag = document.querySelector("#htmltag");
-const toggle = document.querySelector("#toggle");
-
-toggle.addEventListener("click",()=>{
-    htmltag.classList.toggle("dark");
-});
